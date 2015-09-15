92d36d970b67dad96bb6f0834b7fee034a49c8f0
Christos <>
2015-09-15 09:43:51 +0200

    script

---
+++ sctpscript.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cp -a tcptrace-6.6.7 tcptrace-6.6.72
+wait
+cd tcptrace-6.6.72
+wait
+./configure
+wait
+make
+wait
+make install
+wait
+cd ..
+wait
+rm -rf tcptrace-6.6.72
+wait
+cd /usr/local/bin
+wait
+./tcptrace SkypeIRC.cap
